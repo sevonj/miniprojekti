3827a545ccfc0d71be181436ac2931c1ed8d7db6
modar_algayal <mudar.algayal@helsinki.fi>
2023-11-15 13:35:30 +0200

    Badge lisätty

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # miniprojekti
 
+[CodeCov Badge](https://github.com/sevonj/miniprojekti/workflows/<CI>/badge.svg)
+
 
 Miniprojekti on täällä
-
 
 https://tree.taiga.io/project/sevonj-miniprojekti/
--- src/app.py
+++ src/app.py
@@ -2,11 +2,12 @@ from pybtex.database import BibliographyData
 
 
 class App:
+    """main app"""
     def __init__(self):
         print("This is the app")
         self._bib_data = None
 
     def create_bib(self):
         """Load an empty bibliography"""
-
+        print("initializing")
         self._bib_data = BibliographyData()
