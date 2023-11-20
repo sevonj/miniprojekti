28b4192205dcad7c4f68fba80d5d4c7021f4dcc7
sevonj <100710152+sevonj@users.noreply.github.com>
2023-11-20 16:15:31 +0200

    pytest for app

---
+++ src/tests/src/app_test.py
@@ -0,0 +1,19 @@
+"""
+This file containt unit tests for the App class.
+"""
+# pylint: disable=protected-access missing-class-docstring missing-function-docstring
+
+import unittest
+from pybtex.database import BibliographyData
+from app import App
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = App()
+        self.app.create_bib()
+
+    def test_create_bib(self):
+        self.app._bib_data = None
+        self.app.create_bib()
+        self.assertEqual(self.app._bib_data, BibliographyData())
