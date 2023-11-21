8f525fffe7eec826f049036d02f8c183e2cf3e41
Tamás Tóth <tomjtoth@gmail.com>
2023-11-21 14:59:45 +0200

    UI-related funcs(add/list) migrated to main.py

--- src/app.py
+++ src/app.py
@@ -14,40 +14,6 @@ class App:
         """Load an empty bibliography"""
         self._bib_data = BibliographyData()
 
-    def add_entries(self):
-        """Takes user's input fir the article citation"""
-        while True:
-            print("Enter article citation details:")
-            author = input("Author: ")
-            title = input("Title: ")
-            journal = input("Journal: ")
-            year = input("Year: ")
-            volume = input("Volume: ")
-            number = input("Number: ")
-            pages = input("Pages: ")
-
-            if not any([author, title, journal, year, volume, number, pages]):
-                print("An entry is missing, try again.")
-            else:
-                # Create an Entry object representing the article citation
-                entry = Entry(
-                    "article",
-                    fields={
-                        "author": author,
-                        "title": title,
-                        "journal": journal,
-                        "year": year,
-                        "volume": volume,
-                        "number": number,
-                        "pages": pages,
-                    },
-                )
-
-                # Add the entry to the BibliographyData
-                self._bib_data.add_entry("article-minimal", entry)
-                print("Entry successfully saved to the database.")
-                break
-
     def get_entries(self):
         """Get the entries from the bibliography
 
@@ -63,3 +29,7 @@ class App:
 
         except Exception as e:  # pylint: disable=broad-except
             return None, f"Failed to retrieve entries: {e}"
+
+    def add_entry(self, entry: Entry):
+        key = str(len(self._bib_data.entries))
+        self._bib_data.add_entry(key, entry)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,49 @@
 from app import App
+from pybtex.database import Entry
+
+
+def get_entries(app: App):
+    print(app.get_entries())
+
+
+def add_entries(app: App):
+    """Takes user's input for the article citation"""
+    print("Enter article citation details:")
+    author = input("Author: ")
+    title = input("Title: ")
+    journal = input("Journal: ")
+    year = input("Year: ")
+    volume = input("Volume: ")
+    number = input("Number: ")
+    pages = input("Pages: ")
+
+    if not any([author, title, journal, year, volume, number, pages]):
+        print("An entry is missing, try again.")
+        return
+
+    # Create an Entry object representing the article citation
+    entry = Entry(
+        "article",
+        fields={
+            "author": author,
+            "title": title,
+            "journal": journal,
+            "year": year,
+            "volume": volume,
+            "number": number,
+            "pages": pages,
+        },
+    )
+
+    # Add the entry to the BibliographyData
+    app.add_entry(entry)
+    print("Entry successfully saved to the database.")
 
 
 def main():
     app = App()
     app.create_bib()
-    
+
     # App loop
     while True:
         command = input("Enter command (ADD/LIST/EXIT): ").upper().strip()
@@ -12,6 +51,13 @@ def main():
         match command:
             case "EXIT":
                 break
+
+            case "ADD":
+                add_entries(app)
+
+            case "LIST":
+                get_entries(app)
+
             case _:
                 print(f"Unrecognized command: '{command}'")
 
