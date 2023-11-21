9ae1133a95291c23af6edf6853ff8d8952dd6e8a
Miina Saroma <miina.saroma@helsinki.fi>
2023-11-21 16:28:54 +0200

    Test add entry and get entries

--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -7,7 +7,7 @@ This module contains unit tests for the App class.
 # pylint: disable=protected-access missing-class-docstring missing-function-docstring
 
 import unittest
-from pybtex.database import BibliographyData
+from pybtex.database import BibliographyData, Entry
 from app import App
 
 
@@ -20,3 +20,54 @@ class TestApp(unittest.TestCase):
         self.app._bib_data = None
         self.app.create_bib()
         self.assertEqual(self.app._bib_data, BibliographyData())
+
+    def test_add_entry(self):
+        entry = Entry(
+            "article",
+            fields={
+                "author": "Author",
+                "title": "Title",
+                "journal": "Journal",
+                "year": "Year",
+                "volume": "Volume",
+                "number": "Number",
+                "pages": "Pages",
+            },
+        )
+        # no entries yet
+        self.assertEqual(len(self.app._bib_data.entries), 0)
+
+        self.app.add_entry(entry)
+
+        # one entry is added
+        self.assertEqual(len(self.app._bib_data.entries), 1)
+
+        # added entry is equal to the original obj
+        key = list(self.app._bib_data.entries.keys())[0]
+        saved_entry = self.app._bib_data.entries[key]
+        self.assertEqual(entry, saved_entry)
+
+    def test_get_entries(self):
+        entry = Entry(
+            "article",
+            fields={
+                "author": "Author",
+                "title": "Title",
+                "journal": "Journal",
+                "year": "Year",
+                "volume": "Volume",
+                "number": "Number",
+                "pages": "Pages",
+            },
+        )
+
+        # no entries in the beginning
+        self.assertTupleEqual(self.app.get_entries(), (None, "No entries found"))
+
+        self.app.add_entry(entry)
+
+        # entries can be retrieved when there's an added entry
+        self.assertTupleEqual(
+            self.app.get_entries(),
+            (self.app._bib_data.entries, "Successfully retrieved entries"),
+        )
