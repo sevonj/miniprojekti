1fefec39acc0cd31fb7ca73b21ccb5111b2d2409
sevonj <100710152+sevonj@users.noreply.github.com>
2023-11-21 12:54:15 +0200

    remove debug prints

--- src/app.py
+++ src/app.py
@@ -8,12 +8,10 @@ class App:
     """
 
     def __init__(self):
-        print("This is the app")
         self._bib_data = None
 
     def create_bib(self):
         """Load an empty bibliography"""
-        print("initializing")
         self._bib_data = BibliographyData()
 
     def add_entries(self):
