70b810787b0737d0320741f8a7fa0404d5cbb66d
sevonj <100710152+sevonj@users.noreply.github.com>
2023-11-24 13:09:21 +0200

    Injected IO

---
+++ src/app_io.py
@@ -0,0 +1,58 @@
+"""App IO"""
+
+
+class AppIO:
+    """Injected IO handler"""
+
+    def __init__(self) -> None:
+        pass
+
+    def print(self, *args) -> None:
+        """print wrapper"""
+        print(*args)
+
+    def input(self, *args) -> str:
+        """input wrapper"""
+        return input(*args)
+
+
+class StubIO:
+    """Fake IO handler for testing"""
+
+    def __init__(self) -> None:
+        self._outputs = []
+        self._inputs = ["list", "list", "list", "list"]
+
+    def print(self, *args) -> None:
+        """Fake print for the app"""
+
+        # Turn args into string and store them
+        output_text = " ".join(map(str, args))
+        self._outputs.append(output_text)
+        print(self._outputs[-1])
+
+    def input(self, *args) -> str | None:
+        """Fake input for the app"""
+
+        # Turn args into string and store them
+        output_text = " ".join(map(str, args))
+        self._outputs.append(output_text)
+
+        if len(self._inputs) == 0:
+            return None
+        print(self._outputs[-1])
+
+        return self._inputs.pop(0)
+
+    def add_input(self, text: str) -> None:
+        """This allows test script to add inputs to the queue"""
+
+        self._inputs.append(text)
+
+    def pop_output(self) -> str | None:
+        """This allows test script to get outputs from the queue"""
+
+        if len(self._inputs) == 0:
+            return None
+
+        return self._outputs.pop(0)
